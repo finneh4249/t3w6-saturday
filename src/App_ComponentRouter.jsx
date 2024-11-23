0f4e2391d93aa7823737ca4eaca3ae6077c0fa87
Ethan Cornwill <ethancornwill@outlook.com>
2024-11-23 11:08:05 +1100

    Add Navbar component and integrate it into the main App component

--- src/App_ComponentRouter.jsx
+++ src/App_ComponentRouter.jsx
@@ -3,19 +3,22 @@ import './styles/App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+        <h1>Example</h1>
       {/* Routes are declared within this component */}
       <BrowserRouter>
+      <Navbar />
         {/* Declare individual route components here */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> 
     </div>
   );
 }
